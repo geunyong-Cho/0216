7ecd6de53a8bc98662d9ab2372cf16585a748114
geunyong-Cho <gdsrhnd74@naver.com>
2023-02-16 10:41:38 +0900

    1st commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+def changeme(su):
+    print('su = %d' % su)
+    su += 100
+    
+# changeme() # call by name
+num = 5
+changeme(num) # call by value(number, string datatype)
+print('num = %d' % num)
+
